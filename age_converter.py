57ae418fec3ab2825b88c09ab5a86c45b12857e4
devamcodes <devamagrawal.codes@gmail.com>
2020-08-17 13:59:36 +0530

    name changed. it converts years to months.

---
+++ age_converter.py
@@ -0,0 +1,6 @@
+user_input = input("please enter your name:")
+print(f"HELLO, {user_input.title()}")
+user_age = int(input("please enter your age:"))
+final_age = user_age * 12
+print(f"YOUR AGE IS {final_age} MONTHS")
+input ("PROGRAM COMPLETED!!!")
