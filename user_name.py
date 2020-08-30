ba2bd94e68b548febb17445c461aef68e6371f19
devamcodes <devamagrawal.codes@gmail.com>
2020-08-30 15:53:19 +0530

    user name function class created to creat init method for importing to different file but failed so changed to original.

--- user_name.py
+++ user_name.py
@@ -4,8 +4,6 @@ Author: Devam A
 
 Description: if user gives no input (i.e, press enter without typing anything) then error is raised.
 """
-
-
 def user_name_function():
     user_name = input("Enter your name:")
     user_name_boolean = bool(user_name)
