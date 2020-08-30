fbdb614a868101975685741075b025e49f0332dc
devamcodes <devamagrawal.codes@gmail.com>
2020-08-30 13:56:09 +0530

    import functionality improved.

--- user_name.py
+++ user_name.py
@@ -4,11 +4,11 @@ Author: Devam A
 
 Description: if user gives no input (i.e, press enter without typing anything) then error is raised.
 """
-user_name = input("Enter your name:")
-user_name_boolean = bool(user_name)
 
 
 def user_name_function():
+    user_name = input("Enter your name:")
+    user_name_boolean = bool(user_name)
     if user_name_boolean == True:
         print(f"Hello,{user_name.title()}")
     else:
