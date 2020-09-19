87ad2cead310e292c78b63c4678d0ccfbfb2df1c
devamcodes <devamagrawal.codes@gmail.com>
2020-09-19 22:46:28 +0530

    user name module but need to copy in every file.

--- user_name.py
+++ user_name.py
@@ -1,8 +1,10 @@
 """
+    THIS FUNCTION CAN ONLY BE USED IF THIS FILE IS COPIED FROM THIS FOLDER TO THE PROJECT FOLDER.
 Project: user name function
 Author: Devam A
 
 Description: if user gives no input (i.e, press enter without typing anything) then error is raised.
+
 """
 def user_name_function():
     user_name = input("Enter your name:")
